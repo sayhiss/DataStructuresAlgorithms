3dec0f8a640c0b9ce30d493435ae599b57341135
snailky <513514066@qq.com>
2021-04-29 17:15:18 +0800

    第一次提交

---
+++ 数据结构和算法/LinkList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 单链表
+{
+    class LinkNode<T>
+    {
+        public T Data;
+        public LinkNode<T> Next;
+        public LinkNode(T t, LinkNode<T> next)
+        {
+            Data = t;
+            Next = next;
+        }
+    }
+
+    class LinkList<T>
+    {
+        public LinkNode<T> Head;
+        public LinkList(LinkNode<T> head)
+        {
+            Head = head;
+        }
+
+        /// <summary>
+        /// 遍历反转单链表，思路:遍历每一个节点，使该节点的下一个节点为上一个遍历到的节点
+        /// </summary>
+        public static LinkNode<T> ReverseLinkList(LinkNode<T> node)
+        {
+            if (node == null || node.Next == null)
+            {
+                return node;
+            }
+
+            LinkNode<T> prev = null;
+            LinkNode<T> next = null;
+            LinkNode<T> curr = node;
+
+            while (curr != null)
+            {
+                next = curr.Next;
+                curr.Next = prev;
+                prev = curr;
+
+                curr = next;
+            }
+
+            return prev;
+        }
+    }
+}
---
+++ 数据结构和算法/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 单链表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkNode<int> node5 = new LinkNode<int>(5,null);
+            LinkNode<int> node4 = new LinkNode<int>(4, node5);
+            LinkNode<int> node3 = new LinkNode<int>(3, node4);
+            LinkNode<int> node2 = new LinkNode<int>(2, node3);
+            LinkNode<int> node1 = new LinkNode<int>(1, node2);
+            LinkNode<int> result = LinkList<int>.ReverseLinkList(node1);
+            Console.WriteLine("finish..");
+            Console.ReadKey();
+        }
+    }
+}
